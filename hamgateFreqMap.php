3821626d74ea879642fcf6f75f09a0af8d27be7c
John J. McDonough <jjmcd@fedoraproject.org>
2010-06-07 09:04:06 -0400

    Add map of hamgate frequencies

---
+++ hamgateFreqMap.php
@@ -0,0 +1,62 @@
+<?php
+//    freqmap.php
+//    $Revision: 1.0 $ - $Date: 2010-04-12 09:49:55-04 $
+//
+//    Display the frequency map image on a standard page
+//
+    include('includes/session.inc');
+    $title=_('Michigan Hamgate Frequencies');
+
+    include('includes/functions.inc');
+
+    // Remember the launch time
+    $starttime = strftime("%A, %B %d %Y, %H:%M");
+
+    // Open the database
+    $db = mysql_connect($host , $dbuser, $dbpassword);
+    mysql_select_db($DatabaseName,$db);
+
+    ARESheader($title,"ARES RACES ARPSC Michigan Section");
+    ARESleftBar( $db );
+?>
+  <div id="main">
+    <h1><center>Hamgate Frequencies</center></h1>
+      <p><center>
+        <a target="freqMap" href="hamgatefreqs.php">
+          <img src="hamgatefreqs.php" width="460" height="519" />
+        </a>
+        <p>Click on image to enlarge</p>
+<?php
+    $SQL = "SELECT `frequency`,`valid`,A.`updated`,`countyname` " .
+      "FROM `hamgate_freqs` A, `arpsc_counties` B " .
+      "WHERE A.`county` = B.`countycode` AND `valid`=1 " .
+      "ORDER BY A.`county`";
+    $res = getResult( $SQL, $db );
+    echo "      <table>\n";
+    echo "        <tr>\n";
+    echo "          <th>County</th>\n";
+    echo "          <th>Frequency</th>\n";
+    echo "        </tr>\n";
+    while ( $row = getRow( $res ) )
+      {
+	echo "        <tr>\n";
+	echo "          <td align=\"right\">" . $row[3] . "&nbsp;</td>\n";
+	echo "          <td>&nbsp;" . $row[0] . "</td>\n";
+	echo "        </tr>\n";
+      }
+    echo "      </table>\n";
+    echo "      <p>Note that node names are typically hamgate.(county)." .
+         "ampr.org</b>\n";
+    echo "      <p>Refer to <a href=\"http://hamgate.midland.mi-drg.org" .
+         "/ares/mihosts.txt\" target=\"hostlist\">http://hamgate.midlan" .
+         "d.mi-drg.org/ares/mihosts.txt</a> for a complete list of regi" .
+         "stered Michigan packet nodes.</p>\n";
+    echo "      </center></p>\n";
+    echo "  </div>\n";
+    sectLeaders($db);
+    footer($starttime,$maxdate,
+      "\$Revision: 1.0 $ - $Date: 2010-05-09 14:06:43-04 $");
+?>
+</div>
+</body>
+</html>
---
+++ hamgatefreqs.php
@@ -0,0 +1,73 @@
+<?php
+//    countyfreqs.php
+//    $Revision: 1.0 $ - $Date: 2010-04-12 09:33:07-04 $
+//
+//    Create image of state with county frquencies
+//
+
+include('includes/session.inc');
+include('includes/functions.inc');
+
+// Open the database
+$db = mysql_connect($host , $dbuser, $dbpassword);
+mysql_select_db($DatabaseName,$db);
+
+// Create the background wallpaper
+$image = imagecreatefrompng("county_freqs-pl.png");
+
+// Attributes for the frequencies
+$white = ImageColorAllocate( $image, 255,255,255 );
+$black = ImageColorAllocate( $image, 0,0,0 );
+$shadow = ImageColorAllocate( $image, 128, 128, 128 );
+$background = ImageColorAllocate( $image, 224, 224, 224 );
+$d1color = ImageColorAllocate( $image, 255,0,0 );
+$d2color = ImageColorAllocate( $image, 0,128,128 );
+$d3color = ImageColorAllocate( $image, 0,0,192 );
+$d5color = ImageColorAllocate( $image, 192,128,0 );
+$d6color = ImageColorAllocate( $image, 112,176,0 );
+$d7color = ImageColorAllocate( $image, 192,0,192 );
+$freqfont = 5;
+
+// Get the data
+$SQL="SELECT A.`x`,A.`y`,A.`frequency`,B.`district`,A.`valid` " .
+  "FROM `hamgate_freqs` A, `arpsc_counties` B " .
+  "WHERE A.`county`=B.`countycode`";
+$res=getResult($SQL,$db);
+
+// Print the frequencies on the image
+while( $row = getRow( $res ) )
+  {
+    // Only valid data
+    if ( $row[4] )
+      {
+	ImageFilledRectangle( $image, $row[0]-1, $row[1]-1,
+			      $row[0]+56, $row[1]+16,
+			      $background );
+	// Use a separate color for each district
+	$fcolor=$black;
+	if ( $row[3] == 1 )
+	  $fcolor=$d1color;
+	if ( $row[3] == 2 )
+	  $fcolor=$d2color;
+	if ( $row[3] == 3 )
+	  $fcolor=$d3color;
+	if ( $row[3] == 5 )
+	  $fcolor=$d5color;
+	if ( $row[3] == 6 )
+	  $fcolor=$d6color;
+	if ( $row[3] == 7 )
+	  $fcolor=$d7color;
+	// Colored text on top of highlight/shadow
+	ImageString( $image, $freqfont,  $row[0]-2, $row[1]+1, $row[2], $white);
+	ImageString( $image, $freqfont,  $row[0]-1, $row[1]+1, $row[2], $white);
+	ImageString( $image, $freqfont,  $row[0]+1, $row[1]-1, $row[2], $shadow);
+	ImageString( $image, $freqfont,  $row[0]+2, $row[1]-1, $row[2], $shadow);
+	ImageString( $image, $freqfont,  $row[0], $row[1], $row[2], $fcolor);
+      }
+  }
+
+// Finally, actually expose the image
+header('Content-type: image/png');
+ImagePNG($image);
+
+?>
